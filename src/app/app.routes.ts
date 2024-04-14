e306a6e70d98a345e98698fceac32ca19544d2ca
Kyle <kyleperalestudio@gmail.com>
2024-04-15 02:24:54 +0800

    Implement catch all path for incorrect or inaccessible routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { authGuardFn } from '@auth0/auth0-angular';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'weather',
     loadChildren: () => import('./weather/weather.module').then(m => m.WeatherModule),
     canActivate: [authGuardFn],
-  }
+  },
+  { path: '**', redirectTo: '/login' }
 ];
