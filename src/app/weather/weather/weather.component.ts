8a5be7d273d3acb88d6f563557a47fda3a683ea3
Kyle <kyleperalestudio@gmail.com>
2024-04-14 20:13:14 +0800

    Update weather component to include displaying user details

--- src/app/weather/weather/weather.component.ts
+++ src/app/weather/weather/weather.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { AuthService, User } from '@auth0/auth0-angular';
+import { environment as env } from '../../../environment/environment';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
   imports: [FormsModule, RouterLink],
+  providers: [AuthService],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
-export class WeatherComponent {
+export class WeatherComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   city = '';
+  userDetails: User | null = null;
+  gitHubLink = ''
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      if (user) {
+        this.userDetails = user
+        this.gitHubLink = env.gitHubLink + user.nickname
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
   getWeather() {
     if (this.city) {
