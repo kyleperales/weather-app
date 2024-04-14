be87ed2a1003a2e64bfdc436dd148028918feee5
Kyle <kyleperalestudio@gmail.com>
2024-04-14 17:20:58 +0800

    Create weather module and clean up app component template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'weather', pathMatch: 'full' },
+  { path: 'weather', loadChildren: () => import('./weather/weather.module').then(m => m.WeatherModule) }
+];
