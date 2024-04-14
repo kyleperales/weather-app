c42fd8a678c263c25628319d915f264c85f05dc0
Kyle <kyleperalestudio@gmail.com>
2024-04-14 17:36:55 +0800

    Update weather component to have a form for city input

--- src/app/weather/weather/weather.component.ts
+++ src/app/weather/weather/weather.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
 export class WeatherComponent {
 
+  city = '';
+
+  getWeather() {
+    console.log(this.city)
+  }
 }
